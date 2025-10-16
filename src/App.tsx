0d85d1b38fd5e601a2220e120d2121186f44bbbf
namrajp <active99raj@gmail.com>
2025-10-16 14:16:33 +1100

    Call github API and define state as user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import Form from "./components/Form";
 function App() {
   return (
     <div>
-      <p className="font-bold text-center pt-10">Hello, World!</p>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { type FormEvent } from "react";
+
+function Form() {
+  const [username, setUsername] = useState("");
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      alert("User not found");
+    }
+  }
+  return (
+    <form className="w-full max-w-sm mx-auto" onSubmit={handleSubmit}>
+      <div className="flex mt-10">
+        <input
+          name="username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="block w-full px-3 py-2 border border-gray-300 form-input rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
+        />
+        <button
+          type="submit"
+          className="px-3 py-2 ml-2 text-sm font-medium text-zinc-700 border border-gray-300 rounded-md leading-4 hover:text-zinc-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-zinc-50 active:text-zinc-800"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
